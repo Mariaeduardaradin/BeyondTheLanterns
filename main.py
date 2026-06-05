6a9fcd798b10b31e63df1f7b1328c01b4f001919
Maria Eduarda Radin <mariaeduarda0711@outlook.com>
2026-06-05 15:23:34 -0300

    Implementação de obstáculos, vidas, flor, dificuldade progressiva e tela de game over

--- main.py
+++ main.py
@@ -26,12 +26,21 @@ menu = pygame.image.load("bases/menu.jpg")
 menu = pygame.transform.scale(menu, (1000,700))
 fundo = pygame.image.load("bases/fundo.jpg")
 fundo = pygame.transform.scale(fundo, (1000,700))
+gameover = pygame.image.load("bases/gameover.jpg")
+gameover = pygame.transform.scale(gameover, (1000,700))
 pascal = pygame.image.load("bases/pascal.png")
 pascal = pygame.transform.scale(pascal, (105,75))
 lanterna = pygame.image.load("bases/lanterna.png")
 lanterna = pygame.transform.scale(lanterna, (42,60))
 flor = pygame.image.load("bases/flor.png")
-flor = pygame.transform.scale(flor, (40,60))
+flor = pygame.transform.scale(flor, (46,70))
+coracao = pygame.image.load("bases/vidas.png")
+coracao = pygame.transform.scale(coracao, (55,55))
+pedra1 = pygame.image.load("bases/pedra1.png")
+pedra2 = pygame.image.load("bases/pedra2.png")
+arbusto = pygame.image.load("bases/arbusto.png")
+tronco = pygame.image.load("bases/tronco.png")
+listaObstaculos = [pedra1,pedra2,arbusto,tronco]
 
 def tela_inicio():
     maiorNome, maiorPontos, dataJogada, horaJogada = maior_pontuador()
@@ -59,7 +68,7 @@ def tela_inicio():
         tela.blit(titulo, (110, 25))
         textoNome = fonte.render(f"Jogador: {nome}", True, branco)
         tela.blit(textoNome, (50,250))
-        textoRecorde = fonte.render(f"Maior Pontuador: {maiorNome}", True, branco)
+        textoRecorde = fonte.render(f"Recordista: {maiorNome}", True, branco)
         tela.blit(textoRecorde, (50,340))
         textoPontos = fonte.render(f"Pontos: {maiorPontos}", True, branco)
         tela.blit(textoPontos, (50,380))
@@ -81,6 +90,32 @@ def tela_inicio():
         pygame.display.update()
         relogio.tick(60)
 
+def game_over(nome, pontos):
+    salvar_log(nome, pontos)
+    maiorNome, maiorPontos, dataJogada, horaJogada = maior_pontuador()
+
+    fonte = pygame.font.Font("bases/EBGaramond.ttf", 30)
+
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_RETURN:
+                    return
+                if evento.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+        
+        tela.blit(gameover, (0,0))
+        textoMaiorPontuador = fonte.render(f"Maior Pontuador:    {maiorNome}  {maiorPontos} pontos  {dataJogada} - {horaJogada} ", True, branco)
+        textoStartAgain = fonte.render("Pressione ENTER para jogar novamente", True, branco)
+        tela.blit(textoMaiorPontuador, (10, 15))
+        tela.blit(textoStartAgain, (525,630))
+
+        pygame.display.update()
+        relogio.tick(60)
 
 def jogo():
     fundoX1 = 0
@@ -91,11 +126,24 @@ def jogo():
     linhaAtual = 2
     posicaoYPascal = linhas[linhaAtual]
     pontos = 0
-    fonteHUD = pygame.font.Font("bases/EBGaramond.ttf", 30)
+    vidas = 3
+    tempoInvulneravel = 0
+    fonteHUD = pygame.font.Font("bases/EBGaramond.ttf", 25)
     posicaoXLanterna = 1200
     posicaoYLanterna = random.choice(linhas)
+    posicaoXFlor = random.randint(8000,15000)
+    posicaoYFlor = random.choice(linhas)
 
-
+    posicaoXObstaculo1 = random.randint(1000,1200)
+    posicaoYObstaculo1 = random.choice(linhas)
+    posicaoXObstaculo2 = random.randint(1200,1400)
+    posicaoYObstaculo2 = random.choice(linhas)
+    posicaoXObstaculo3 = random.randint(1400,1700)
+    posicaoYObstaculo3 = random.choice(linhas)
+    obstaculoAtual1 = random.choice(listaObstaculos)
+    obstaculoAtual2 = random.choice(listaObstaculos)
+    obstaculoAtual3 = random.choice(listaObstaculos)
+    
     while True:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
@@ -114,41 +162,155 @@ def jogo():
 
 
         posicaoYPascal = linhas[linhaAtual]
+        if tempoInvulneravel > 0:
+            tempoInvulneravel -= 1
         retanguloPascal = pygame.Rect(posicaoXPascal, posicaoYPascal, 105,75)
         retanguloLanterna = pygame.Rect(posicaoXLanterna, posicaoYLanterna, 42,60)
+        retanguloObstaculo1 = pygame.Rect(posicaoXObstaculo1,posicaoYObstaculo1, 55,55)
+        retanguloObstaculo2 = pygame.Rect(posicaoXObstaculo2, posicaoYObstaculo2, 55,55)
+        retanguloObstaculo3 = pygame.Rect(posicaoXObstaculo3, posicaoYObstaculo3, 55,55)
+        retanguloFlor = pygame.Rect(posicaoXFlor, posicaoYFlor, 40,60)
 
         if retanguloPascal.colliderect(retanguloLanterna):
             pontos += 1
-            posicaoXLanterna = random.randint(1200,1800)
+            posicaoXLanterna = random.randint(1000,1500)
             posicaoYLanterna = random.choice(linhas)
-        if pontos >= 10:
+            while ((posicaoYLanterna == posicaoYObstaculo1 and abs(posicaoXLanterna - posicaoXObstaculo1) < 250) or (posicaoYLanterna == posicaoYObstaculo2 and abs(posicaoXLanterna - posicaoXObstaculo2) < 250) or (posicaoYLanterna == posicaoYObstaculo3 and abs(posicaoXLanterna - posicaoXObstaculo3) < 250) or (posicaoYLanterna == posicaoYFlor and abs(posicaoXLanterna - posicaoXFlor) < 250)):
+                posicaoXLanterna = random.randint(1000,1500)
+                posicaoYLanterna = random.choice(linhas)
+        if retanguloPascal.colliderect(retanguloFlor):
+            pontos += 5
+            posicaoXFlor = random.randint(8000,15000)
+            posicaoYFlor = random.choice(linhas)
+            while ((posicaoYFlor == posicaoYObstaculo1 or posicaoYFlor == posicaoYObstaculo2 or posicaoYFlor == posicaoYObstaculo3) or (posicaoYFlor == posicaoYLanterna and abs(posicaoXFlor - posicaoXLanterna) < 250)):
+                posicaoXFlor = random.randint(8000,15000)
+                posicaoYFlor = random.choice(linhas)
+        if (retanguloPascal.colliderect(retanguloObstaculo1) and tempoInvulneravel == 0):
+            vidas -= 1
+            tempoInvulneravel = 60
+            if linhaAtual == 0:
+                linhaAtual += 1
+            elif linhaAtual == 4:
+                linhaAtual -= 1
+            else:
+                direcao = random.choice([-1,1])
+                linhaAtual += direcao
+        if (retanguloPascal.colliderect(retanguloObstaculo2) and tempoInvulneravel == 0):
+            vidas -= 1
+            tempoInvulneravel = 60
+            if linhaAtual == 0:
+                linhaAtual += 1
+            elif linhaAtual == 4:
+                linhaAtual -= 1
+            else:
+                direcao = random.choice([-1,1])
+                linhaAtual += direcao
+        if (retanguloPascal.colliderect(retanguloObstaculo3) and tempoInvulneravel == 0):
+            vidas -= 1
+            tempoInvulneravel = 60
+            if linhaAtual == 0:
+                linhaAtual += 1
+            elif linhaAtual == 4:
+                linhaAtual -= 1
+            else:
+                direcao = random.choice([-1,1])
+                linhaAtual += direcao
+        if vidas <= 0:
+            game_over(nome, pontos)
+            return
+                            
+        if pontos >= 5:
             velocidadeFundo = 4
-        if pontos >= 20:
+        if pontos >= 12:
             velocidadeFundo = 5
-        if pontos >= 30:
+        if pontos >= 20:
             velocidadeFundo = 6
+        if pontos >= 30:
+            velocidadeFundo = 7
+        if pontos >= 42:
+            velocidadeFundo = 8
+        if pontos >= 55:
+            velocidadeFundo = 9
 
         fundoX1 -= velocidadeFundo
         fundoX2 -= velocidadeFundo
         posicaoXLanterna -= velocidadeFundo
+        posicaoXObstaculo1 -= velocidadeFundo
+        posicaoXObstaculo2 -= velocidadeFundo
+        posicaoXObstaculo3 -= velocidadeFundo
+        posicaoXFlor -= velocidadeFundo
         if fundoX1 <= -1000:
             fundoX1 = 1000
         if fundoX2 <= -1000:
             fundoX2 = 1000
         if posicaoXLanterna < -100:
-            posicaoXLanterna = random.randint(1200,1800)
+            posicaoXLanterna = random.randint(1000,1500)
             posicaoYLanterna = random.choice(linhas)
+            while ((posicaoYLanterna == posicaoYObstaculo1 and abs(posicaoXLanterna - posicaoXObstaculo1) < 250) or (posicaoYLanterna == posicaoYObstaculo2 and abs(posicaoXLanterna - posicaoXObstaculo2) < 250) or (posicaoYLanterna == posicaoYObstaculo3 and abs(posicaoXLanterna - posicaoXObstaculo3) < 250) or (posicaoYLanterna == posicaoYFlor and abs(posicaoXLanterna - posicaoXFlor) < 250)):
+                posicaoXLanterna = random.randint(1000,1500)
+                posicaoYLanterna = random.choice(linhas)
+        if posicaoXObstaculo1 < -100:
+            posicaoXObstaculo1 = random.randint(1000,1200)
+            linhaObstaculo = random.choice(linhas)
+            if random.randint(1,100) <= 75:
+                while (linhaObstaculo == posicaoYObstaculo2 or linhaObstaculo == posicaoYObstaculo3):
+                    linhaObstaculo = random.choice(linhas)
+            while (linhaObstaculo == posicaoYLanterna or linhaObstaculo == posicaoYFlor):
+                linhaObstaculo = random.choice(linhas)
+            posicaoYObstaculo1 = linhaObstaculo
+            obstaculoAtual1 = random.choice(listaObstaculos)
+        if posicaoXObstaculo2 < -100:
+            posicaoXObstaculo2 = random.randint(1150,1350)
+            linhaObstaculo = random.choice(linhas)
+            if random.randint(1,100) <= 75:
+                while (linhaObstaculo == posicaoYObstaculo1 or linhaObstaculo == posicaoYObstaculo3):
+                    linhaObstaculo = random.choice(linhas)
+            while linhaObstaculo == posicaoYObstaculo1:
+                linhaObstaculo = random.choice(linhas)
+            while (linhaObstaculo == posicaoYLanterna or linhaObstaculo == posicaoYFlor):
+                linhaObstaculo = random.choice(linhas)
+            posicaoYObstaculo2 = linhaObstaculo
+            obstaculoAtual2 = random.choice(listaObstaculos)
+        if posicaoXObstaculo3 < -100:
+            posicaoXObstaculo3 = random.randint(1300,1600)
+            linhaObstaculo = random.choice(linhas)
+            while (linhaObstaculo == posicaoYObstaculo1 or linhaObstaculo == posicaoYObstaculo2):
+                    linhaObstaculo = random.choice(linhas)
+            if random.randint(1,100) <= 75:
+                while (linhaObstaculo == posicaoYObstaculo1 or linhaObstaculo == posicaoYObstaculo2):
+                    linhaObstaculo = random.choice(linhas)
+            while (linhaObstaculo == posicaoYLanterna or linhaObstaculo == posicaoYFlor):
+                linhaObstaculo = random.choice(linhas)
+            posicaoYObstaculo3 = linhaObstaculo
+            obstaculoAtual3 = random.choice(listaObstaculos)
+        if posicaoXFlor < -100:
+            posicaoXFlor = random.randint(8000,15000)
+            posicaoYFlor = random.choice(linhas)
+            while ((posicaoYFlor == posicaoYObstaculo1 and abs(posicaoXFlor - posicaoXObstaculo1) < 250) or (posicaoYFlor == posicaoYObstaculo2 and abs(posicaoXFlor - posicaoXObstaculo2) < 250) or (posicaoYFlor == posicaoYObstaculo3 and abs(posicaoXFlor - posicaoXObstaculo3) < 250) or (posicaoYFlor == posicaoYLanterna and abs(posicaoXFlor - posicaoXLanterna) < 250)):
+                posicaoXFlor = random.randint(8000,15000)
+                posicaoYFlor = random.choice(linhas)
 
         tela.blit(fundo, (fundoX1,0))
         tela.blit(fundo, (fundoX2,0))
         tela.blit(lanterna, (posicaoXLanterna, posicaoYLanterna))
-        tela.blit(pascal, (posicaoXPascal, posicaoYPascal))
+        tela.blit(obstaculoAtual1, (posicaoXObstaculo1, posicaoYObstaculo1))
+        tela.blit(obstaculoAtual2, (posicaoXObstaculo2, posicaoYObstaculo2))
+        tela.blit(obstaculoAtual3, (posicaoXObstaculo3, posicaoYObstaculo3))
+        tela.blit(flor, (posicaoXFlor, posicaoYFlor))
+        if tempoInvulneravel == 0:
+            tela.blit(pascal, (posicaoXPascal, posicaoYPascal))
+        else:
+            if tempoInvulneravel % 10 < 5:
+                tela.blit(pascal, (posicaoXPascal, posicaoYPascal))
         textoPontos = fonteHUD.render(f"Pontos: {pontos}", True, branco)
         tela.blit(textoPontos, (20,20))
+        tela.blit(coracao, (915,10))
+        textoVida = fonteHUD.render(f"{vidas}", True, branco)
+        tela.blit(textoVida, (936,18))
 
         pygame.display.update()
         relogio.tick(60)
 
-
-tela_inicio()    
-jogo()
+while True:
+    tela_inicio()
+    jogo()
